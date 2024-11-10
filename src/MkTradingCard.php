8c0672d7c9637658442af29c838709ab42fba8fe
morentic <mkopp4191@gmail.com>
2024-11-10 12:17:09 +0100

    add symfony bundle to customize shopware

---
+++ src/MkTradingCard.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MkTradingCard;
+
+use Shopware\Core\Framework\Bundle;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class MkTradingCard extends Bundle implements ThemeInterface
+{
+
+}
